747de2eddeed2cd5c3e5347284049a6208d00cb0
magicdawn <magicdawn@qq.com>
2015-09-04 21:24:16 +0800

    app.js expose app

--- app.js
+++ app.js
@@ -78,12 +78,3 @@ if (app.env === 'production') {
  * index.js
  */
 predator.loadAllRouter();
-
-/**
- * here we go
- */
-
-var port = process.env.PORT || 4000;
-app.listen(port, function() {
-  console.log('predator demo runing at http://localhost:%s', this.address().port);
-});
